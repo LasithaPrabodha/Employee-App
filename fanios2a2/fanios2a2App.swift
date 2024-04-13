94acaf6377c4ccd615f6758562f454268cafb860
Lasitha Weligampola <lasiprabo@gmail.com>
2024-04-13 10:52:36 -0400

    Initial Commit

---
+++ fanios2a2/fanios2a2App.swift
@@ -0,0 +1,17 @@
+//
+//  fanios2a2App.swift
+//  fanios2a2
+//
+//  Created by Lasitha Weligampola on 2024-04-13.
+//
+
+import SwiftUI
+
+@main
+struct fanios2a2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
